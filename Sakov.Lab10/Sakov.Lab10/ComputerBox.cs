659fe462d020922cdfa1371b223fe63add6977c8
sakov00 <54810347+sakov00@users.noreply.github.com>
2020-09-28 12:20:43 +0300

    Add files via upload

---
+++ Sakov.Lab10/Sakov.Lab10/ComputerBox.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Sakov.Lab10
+{
+    /// <summary>
+    /// Выполните шаги 1a или 1b, а затем 2, чтобы использовать этот пользовательский элемент управления в файле XAML.
+    ///
+    /// Шаг 1a. Использование пользовательского элемента управления в файле XAML, существующем в текущем проекте.
+    /// Добавьте атрибут XmlNamespace в корневой элемент файла разметки, где он 
+    /// будет использоваться:
+    ///
+    ///     xmlns:MyNamespace="clr-namespace:Sakov.Lab10"
+    ///
+    ///
+    /// Шаг 1б. Использование пользовательского элемента управления в файле XAML, существующем в другом проекте.
+    /// Добавьте атрибут XmlNamespace в корневой элемент файла разметки, где он 
+    /// будет использоваться:
+    ///
+    ///     xmlns:MyNamespace="clr-namespace:Sakov.Lab10;assembly=Sakov.Lab10"
+    ///
+    /// Потребуется также добавить ссылку из проекта, в котором находится файл XAML,
+    /// на данный проект и пересобрать во избежание ошибок компиляции:
+    ///
+    ///     Щелкните правой кнопкой мыши нужный проект в обозревателе решений и выберите
+    ///     "Добавить ссылку"->"Проекты"->[Поиск и выбор проекта]
+    ///
+    ///
+    /// Шаг 2)
+    /// Теперь можно использовать элемент управления в файле XAML.
+    ///
+    ///     <MyNamespace:UserElementTextBox/>
+    ///
+    /// </summary>
+    public class ComputerBox : TextBox
+    {
+        public static readonly DependencyProperty PriceProperty;
+        static ComputerBox()
+        {
+            FrameworkPropertyMetadata metadata = new FrameworkPropertyMetadata();
+            metadata.CoerceValueCallback = new CoerceValueCallback(CorrectValue);
+            PriceProperty = DependencyProperty.Register("Price", typeof(int), typeof(ComputerBox), metadata,
+                new ValidateValueCallback(ValidateValue));
+        }
+        private static bool ValidateValue(object value)
+        {
+            int currentValue = (int)value;
+            if (currentValue == 0 || currentValue >= 200) // если текущее значение от нуля и выше
+                return true;
+            return false;
+        }
+        private static object CorrectValue(DependencyObject d, object baseValue)
+        {
+            int currentValue = (int)baseValue;
+            if (currentValue > 1000)  // если больше 1000, возвращаем 1000
+                return 1000;
+            return currentValue; // иначе возвращаем текущее значение
+        }
+        public int Price
+        {
+            get { return (int)GetValue(PriceProperty); }
+            set { SetValue(PriceProperty, value); }
+        }
+
+    }
+}
